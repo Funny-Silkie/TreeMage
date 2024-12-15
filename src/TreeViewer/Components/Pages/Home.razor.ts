26f2dc9af8cf46e332003e07d548a8c36720082c
Funny_Silkie <funnysilkie@gmail.com>
2024-12-15 15:20:21 +0900

    refactor: migrate type definition

--- src/TreeViewer/Components/Pages/Home.razor.ts
+++ src/TreeViewer/Components/Pages/Home.razor.ts
@@ -47,15 +47,3 @@ export function getTextSize(id: string): [number, number] {
     const bBox: DOMRect = element.getBBox();
     return [bBox.width, bBox.height];
 }
-
-/**
- * .NETのRazorコンポーネントを表します。
- */
-interface DotNetRazorComponent {
-    /**
-     * 指定したメソッドを実行します。
-     * @param name メソッド名
-     * @param args 引数
-     */
-    invokeMethodAsync(name: string, ...args: any[]): Promise<void>;
-}
